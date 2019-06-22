075170b4a18c12bba827e7312c65a24288857290
Трефилов Дмитрий <trefilowdem@mail.ru>
2019-06-22 10:22:29 +0500

    Add function CreatePosl

--- Posledovatelnost/Posledovatelnost/Program.cs
+++ Posledovatelnost/Posledovatelnost/Program.cs
@@ -9,8 +9,24 @@ namespace Posledovatelnost
 {
     class Program
     {
+        private static int[] Posl;
+        private static int n;
+        private static void CreatePosl()
+        {
+            ColorMess.Yellow("\n Введите колличество чисел в последовательности (минимум 2): ");
+            n = Input.Check(2, int.MaxValue);
+            Posl = new int[n];
+            for(int i = 0; i<n; i++)
+            {
+                Console.Clear();
+                ColorMess.Yellow("\n Введите " + (i+1) + " число последовательности: ");
+                Posl[i] = Input.Check(int.MinValue, int.MaxValue);
+            }
+        }
         static void Main()
         {
+            CreatePosl();
+            ColorMess.Yellow("\n Ваша новая последовательность выглядит так: ");
 
         }
     }
