4c875d7b5f24e00a6e0bc9f09b4a6086d2b6e13d
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-26 01:11:53 +0500

    Add comments

--- Posledovatelnost/Posledovatelnost/Program.cs
+++ Posledovatelnost/Posledovatelnost/Program.cs
@@ -9,8 +9,11 @@ namespace Posledovatelnost
 {
     class Program
     {
+        #region Колличество элементов последовательности и массив с ней
         private static int[] Posl;
         private static int n;
+        #endregion
+        #region Создает последовательность
         private static void CreatePosl()
         {
             ColorMess.Yellow("\n Введите колличество чисел в последовательности (от 2 до 10): ");
@@ -23,16 +26,22 @@ namespace Posledovatelnost
                 Posl[i] = Input.Check(int.MinValue, int.MaxValue);
             }
         }
+        #endregion
+        #region Печатает последовательность
         private static void PrintPosl()
         {
             for (int i = 0; i < n; i++)
                 ColorMess.White(" " + Posl[i]);
         }
+        #endregion
+        #region Массив для хранения элементов
         private static void UpdatePosl()
         {
             for(int i = 0; i<n-1; i++)
                 Posl[i] = Posl[i] - Posl[n-1];
         }
+        #endregion
+        #region Вызывает функции для создания, обработки и печати последовательности
         static void Main()
         {
             CreatePosl();
@@ -42,5 +51,6 @@ namespace Posledovatelnost
             PrintPosl();
             Message.GoToBack();
         }
+        #endregion
     }
 }
